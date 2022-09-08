ada815ab1933dcc64fad068cf589d889a10320f0
darthleonard <leolinuxmx@gmail.com>
2022-09-07 23:34:03 -0500

    adding default date value when creating new list

--- MyExpensesApp/src/app/pages/shopping/shopping-list-modal/shopping-list-metadata.service.ts
+++ MyExpensesApp/src/app/pages/shopping/shopping-list-modal/shopping-list-metadata.service.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Injectable } from '@angular/core';
 
 import { of } from 'rxjs';
@@ -22,6 +23,7 @@ export class ShoppingListMetadataService {
       new DateControl({
         key: 'effectiveDate',
         label: 'Date',
+        value: new DatePipe('en-US').transform(new Date(), 'MMMM d, y'),
         order: 2
       }),
     ];
