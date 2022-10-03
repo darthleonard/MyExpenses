eae1ea0157a34b351bf6349cc80d255141f58489
darthleonard <leolinuxmx@gmail.com>
2022-10-02 22:53:41 -0500

    adding appId

--- MyExpensesApp/capacitor.config.ts
+++ MyExpensesApp/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.darthleonard.myexpensesapp',
   appName: 'MyExpensesApp',
   webDir: 'www',
   bundledWebRuntime: false
